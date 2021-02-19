7c7374ce19dac30d744c617160c6883832166fec
Meyvi <meyvim@gmail.com>
2021-02-18 21:08:20 -0800

    Sets up react with webpack watch and basic express server that serves html

---
+++ challenge_2/client/components/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is App.</h2>
+      </div>
+    );
+  }
+};
+
+export default App;
---
+++ challenge_2/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'client', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'client'),
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": "defaults"
+              }],
+              '@babel/preset-react'
+            ]
+          }
+        }]
+      }
+    ]
+  }
+}
