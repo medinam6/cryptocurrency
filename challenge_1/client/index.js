6a0af079da82f869717e24b76123520a05eac954
Meyvi Medina-Magarino <meyvi@Meyvis-iMac.local>
2021-04-06 19:36:58 -0700

    Removes umrelated code

--- challenge_1/client/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './components/app.jsx';
-
-ReactDOM.render(
-  React.createElement('div', null, <App />),
-  document.getElementById('index')
-);
